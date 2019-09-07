29907558dcc59f88659a6be3ff920f84bf7af6cd
John Brewer <jbrewer@deepcoredata.com>
2019-09-07 14:42:13 -0400

    Added TechnologyBase to ArmorType.

--- BattleTech.NET/TotalWarfare/ArmorType.cs
+++ BattleTech.NET/TotalWarfare/ArmorType.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BattleTechNET.Common;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -12,10 +13,11 @@ namespace BattleTechNET.TotalWarfare
     /// </summary>
     public class ArmorType
     {
-        public ArmorType():this("Standard",16,0) { }
-        public ArmorType(string sName, double dPointsPerTons) : this(sName, dPointsPerTons, 0) { }
-        public ArmorType(string sName, double dPointsPerTons, int iCriticalSlotsRequired) { Name = sName; PointsPerTon = dPointsPerTons; CriticalSlotsRequired = iCriticalSlotsRequired; }
+        public ArmorType():this("Standard",TECHNOLOGY_BASE.BOTH, 16,0) { }
+        public ArmorType(string sName, TECHNOLOGY_BASE tb, double dPointsPerTons) : this(sName, tb, dPointsPerTons, 0) { }
+        public ArmorType(string sName, TECHNOLOGY_BASE tb, double dPointsPerTons, int iCriticalSlotsRequired) { Name = sName; PointsPerTon = dPointsPerTons; CriticalSlotsRequired = iCriticalSlotsRequired; }
         public string Name { get; set; }
+        public TECHNOLOGY_BASE TechnologyBase { get; set; }
         public double PointsPerTon { get; set; }
         /// <summary>
         /// This is the number of critical slots a Mech must dedicate to having
@@ -31,22 +33,22 @@ namespace BattleTechNET.TotalWarfare
         public static List<ArmorType> CanonicalArmorTypes()
         {
             List<ArmorType> retval = new List<ArmorType>();
-
-            retval.Add(new ArmorType("Standard", 16));                      //TM56
-            retval.Add(new ArmorType("Heavy Industrial", 16));              //TM56
-            retval.Add(new ArmorType("Stealth", 16, 12));                   //TM56
-            retval.Add(new ArmorType("Light Ferro-Fibrous", 16*1.06,7));    //TM56
-            retval.Add(new ArmorType("Ferro-Fibrous (I.S.)", 16 * 1.12, 14));   //TM56
-            retval.Add(new ArmorType("Ferro-Fibrous (Clan)", 16 * 1.2, 7));     //TM56
-            retval.Add(new ArmorType("Heavy Ferro-Fibrous", 16 * 1.24, 21));    //TM56
-            retval.Add(new ArmorType("Ferro-Lemellor",14,2));    //TO280
-            retval.Add(new ArmorType("Hardened", 8, 2));    //TO280
-            retval.Add(new ArmorType("Laser-Reflective (I.S.)", 16, 10));    //TO280
-            retval.Add(new ArmorType("Laser-Reflective (Clan)", 16, 5));    //TO280
-            retval.Add(new ArmorType("Modular", 10, 1));    //TO280
-            retval.Add(new ArmorType("Reactive (I.S.)", 16, 14));    //TO280
-            retval.Add(new ArmorType("Reactive (Clan)", 16, 7));    //TO280
-            retval.Add(new ArmorType("Vehicle Stealth", 16, 7));    //TO280
+            //TODO: Verify Technology Bases on Armors
+            retval.Add(new ArmorType("Standard", TECHNOLOGY_BASE.BOTH, 16));                      //TM56
+            retval.Add(new ArmorType("Heavy Industrial", TECHNOLOGY_BASE.BOTH, 16));              //TM56
+            retval.Add(new ArmorType("Stealth", TECHNOLOGY_BASE.INNERSPHERE, 16, 12));                   //TM56
+            retval.Add(new ArmorType("Light Ferro-Fibrous", TECHNOLOGY_BASE.INNERSPHERE, 16*1.06,7));    //TM56
+            retval.Add(new ArmorType("Ferro-Fibrous (I.S.)", TECHNOLOGY_BASE.INNERSPHERE, 16 * 1.12, 14));   //TM56
+            retval.Add(new ArmorType("Ferro-Fibrous (Clan)", TECHNOLOGY_BASE.CLAN, 16 * 1.2, 7));     //TM56
+            retval.Add(new ArmorType("Heavy Ferro-Fibrous", TECHNOLOGY_BASE.INNERSPHERE, 16 * 1.24, 21));    //TM56
+            retval.Add(new ArmorType("Ferro-Lemellor", TECHNOLOGY_BASE.BOTH, 14,2));    //TO280
+            retval.Add(new ArmorType("Hardened", TECHNOLOGY_BASE.BOTH, 8, 2));    //TO280
+            retval.Add(new ArmorType("Laser-Reflective (I.S.)", TECHNOLOGY_BASE.INNERSPHERE, 16, 10));    //TO280
+            retval.Add(new ArmorType("Laser-Reflective (Clan)", TECHNOLOGY_BASE.CLAN, 16, 5));    //TO280
+            retval.Add(new ArmorType("Modular", TECHNOLOGY_BASE.BOTH, 10, 1));    //TO280
+            retval.Add(new ArmorType("Reactive (I.S.)", TECHNOLOGY_BASE.INNERSPHERE, 16, 14));    //TO280
+            retval.Add(new ArmorType("Reactive (Clan)", TECHNOLOGY_BASE.CLAN, 16, 7));    //TO280
+            retval.Add(new ArmorType("Vehicle Stealth", TECHNOLOGY_BASE.BOTH, 16, 7));    //TO280
 
             return retval;
         }
