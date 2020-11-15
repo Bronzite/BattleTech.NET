140bde83084c932b3121a768f74d1578b044da4b
John Brewer <jbrewer@deepcoredata.com>
2020-11-15 16:04:48 -0500

    Added IS and Mixed Mass Tests.

--- BattleTechNETTest/MassTesting.cs
+++ BattleTechNETTest/MassTesting.cs
@@ -122,5 +122,76 @@ namespace BattleTechNETTest
             Assert.Equal(iClanMechCount, iLoadCount);
         }
 
+        [Fact(DisplayName = "Inner Sphere Mass Check MegaMek Files")]
+        public void MassCheckISMegaMekFiles()
+        {
+            string[] sFiles = GetFiles(sDirectory, "*.mtf");
+            int iLoadCount = 0;
+            int iInnerSphereMechCount = 0;
+            if (sFiles == null) return;
+            foreach (string sFile in sFiles)
+            {
+                try
+                {
+                    BattleMechDesign bmd = MTFReader.ReadBattleMechDesignFile(sFile);
+                    if (bmd.TechnologyBase == BattleTechNET.Common.TECHNOLOGY_BASE.INNERSPHERE)
+                    {
+                        double dComputedTonnage = bmd.ComputedTonnage;
+                        if ((dComputedTonnage - bmd.Tonnage) > 0.00005 || bmd.Tonnage - dComputedTonnage > 0.5)
+                        {
+                            _outputHelper.WriteLine($"{bmd.Variant} {bmd.Model} rated at {bmd.Tonnage}, computed as {bmd.ComputedTonnage}");
+                            if (bmd.Tonnage < dComputedTonnage) _outputHelper.WriteLine("Computed tonnage exceeds nominal tonnage.");
+                            if (bmd.Tonnage - dComputedTonnage > 0.5) _outputHelper.WriteLine("Computed tonnage more than 0.5 tons below nominal tonnage.");
+                            _outputHelper.WriteLine(bmd.TonnageLedger);
+                        }
+                        else
+                            iLoadCount++;
+                        iInnerSphereMechCount++;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Error loading {sFile}", ex);
+                }
+            }
+            _outputHelper.WriteLine($"Checked {sFiles.Length} MTF files.");
+            Assert.Equal(iInnerSphereMechCount, iLoadCount);
+        }
+
+        [Fact(DisplayName = "Mixed Techbase Mass Check MegaMek Files")]
+        public void MassCheckMixedTechbaseMegaMekFiles()
+        {
+            string[] sFiles = GetFiles(sDirectory, "*.mtf");
+            int iLoadCount = 0;
+            int iMixedTechBaseMechCount = 0;
+            if (sFiles == null) return;
+            foreach (string sFile in sFiles)
+            {
+                try
+                {
+                    BattleMechDesign bmd = MTFReader.ReadBattleMechDesignFile(sFile);
+                    if (bmd.TechnologyBase == BattleTechNET.Common.TECHNOLOGY_BASE.BOTH)
+                    {
+                        double dComputedTonnage = bmd.ComputedTonnage;
+                        if ((dComputedTonnage - bmd.Tonnage) > 0.00005 || bmd.Tonnage - dComputedTonnage > 0.5)
+                        {
+                            _outputHelper.WriteLine($"{bmd.Variant} {bmd.Model} rated at {bmd.Tonnage}, computed as {bmd.ComputedTonnage}");
+                            if (bmd.Tonnage < dComputedTonnage) _outputHelper.WriteLine("Computed tonnage exceeds nominal tonnage.");
+                            if (bmd.Tonnage - dComputedTonnage > 0.5) _outputHelper.WriteLine("Computed tonnage more than 0.5 tons below nominal tonnage.");
+                            _outputHelper.WriteLine(bmd.TonnageLedger);
+                        }
+                        else
+                            iLoadCount++;
+                        iMixedTechBaseMechCount++;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Error loading {sFile}", ex);
+                }
+            }
+            _outputHelper.WriteLine($"Checked {sFiles.Length} MTF files.");
+            Assert.Equal(iMixedTechBaseMechCount, iLoadCount);
+        }
     }
 }
